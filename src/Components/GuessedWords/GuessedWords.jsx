6bf4ac7b1883a94f2212dacee21b57320029b9c0
Pratik Rajendra Valvi <pratikvalvi2305@gmail.com>
2021-06-27 16:41:46 +0530

    GuessedWords component added & tested

--- src/Components/GuessedWords/GuessedWords.jsx
+++ src/Components/GuessedWords/GuessedWords.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 
 const GuessedWords = (props) => {
-    
+
     let contents;
 
     if (props.guessedWords.length === 0) {
+
         contents = (
             <span className='guess-word-message'>
                 Try to guess the Secret Word!!!
             </span>
+        )
+
+    } else {
+
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
+            <tr id={index} className='guessed-word'>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+
+        contents = (
+            <div className='guessed-words'>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Guess</td>
+                            <td>Matching Letters</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { guessedWordsRows }
+                    </tbody>
+                </table>
+            </div>
         )
     }
 
--- src/Components/GuessedWords/GuessedWords.test.js
+++ src/Components/GuessedWords/GuessedWords.test.js
@@ -5,11 +5,11 @@ import GuessedWords from "./GuessedWords";
 describe('Tests for GuessedWords component & guess words message ', () => {
 
     let wrapper;
-    
+
     beforeEach(() => {
-        wrapper = shallow(<GuessedWords guessedWords={[]}/>)
+        wrapper = shallow(<GuessedWords guessedWords={[]} />)
     });
-    
+
     test("GuessedWords component renders without error", () => {
         const component = wrapper.find("div.guessed-words-wrapper");
         expect(component.length).toBe(1);
@@ -18,5 +18,30 @@ describe('Tests for GuessedWords component & guess words message ', () => {
     test("GuessedWords component renders guess words message when guessedWords array is empty", () => {
         const component = wrapper.find("span.guess-word-message").text();
         expect(component.length).not.toBe(0);
+    });
+});
+
+describe('Tests for Guessed Words Section', () => {
+
+    let wrapper;
+
+    const guessedWords = [
+        { guessedWord: 'agile', letterMatchCount: 1 },
+        { guessedWord: 'party', letterMatchCount: 5 },
+        { guessedWord: 'rain', letterMatchCount: 2 }
+    ];
+
+    beforeEach(() => {
+        wrapper = shallow(<GuessedWords guessedWords={guessedWords}/>)
+    });
+
+    test('Guessed Words Section renders without error', () => {
+        const component = wrapper.find("div.guessed-words");
+        expect(component.length).toBe(1);
+    });
+
+    test('Guessed Words Section renders correct number of guessed words', () => {
+        const guessedWordNodes = wrapper.find(".guessed-word");
+        expect(guessedWordNodes.length).toBe(guessedWords.length);
     });
 });
